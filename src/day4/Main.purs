1dc619d5bf42589076c321446822b2d29d08b877
DESKTOP-RLP4NHG\minec <john.mcguinness@ocsarts.net>
2020-12-05 23:32:43 -0800

    Completed day4 part 1

--- src/day4/Main.purs
+++ src/day4/Main.purs
@@ -3,11 +3,11 @@ module Main where
 import Prelude
 
 import Control.MonadZero (guard)
-import Data.Array as Array
+import Data.Array as Data.Array
 import Data.Int as Int
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.String.CodeUnits (toCharArray)
-import Data.String.Utils (lines)
+import Data.String.Utils (fromCharArray, includes, lines)
 import Data.String.Utils as String.Utils
 import Effect (Effect)
 import Effect.Class.Console (log, logShow)
@@ -20,10 +20,40 @@ main = do
 
   let 
     input :: Array String
-    input = String.Utils.lines content
+    input = (String.Utils.lines content) <> [""]
 
+    beforeempty :: Array String -> Array String
+    beforeempty arr = 
+      if fromMaybe "" (Data.Array.head arr) == "" 
+        then []
+        else  [ (fromMaybe "" (Data.Array.head arr)) ] <>  beforeempty (fromMaybe [""] (Data.Array.tail arr))
     
-  logShow 
+    afterempty :: Array String -> Maybe (Array String)
+    afterempty arr =
+      if fromMaybe "" (Data.Array.head arr) == ""
+        then Data.Array.tail arr
+        else afterempty (fromMaybe [""] (Data.Array.tail arr))
+
+    splitintopassports :: Maybe (Array String) -> Array (Array String)
+    splitintopassports Nothing = []
+    splitintopassports (Just arr) = [(beforeempty arr)] <> (splitintopassports (afterempty arr))
+      
+    validatedpassports :: Array (Array String) -> Array (Array String)
+    validatedpassports arr = do
+      i <- arr
+
+      guard (includes ("byr") (fromCharArray i))
+      guard (includes ("iyr") (fromCharArray i))
+      guard (includes ("eyr") (fromCharArray i))
+      guard (includes ("hgt") (fromCharArray i))
+      guard (includes ("hcl") (fromCharArray i))
+      guard (includes ("ecl") (fromCharArray i))
+      guard (includes ("pid") (fromCharArray i))
+
+      pure i
+
+
+  logShow $ Data.Array.length $ validatedpassports $ splitintopassports (Just input)
   pure unit 
 
   
