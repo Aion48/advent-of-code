72cff7e2ad090ee7e11fa87b857b5905dfd44d11
DESKTOP-RLP4NHG\minec <john.mcguinness@ocsarts.net>
2021-01-24 00:43:09 -0800

    completed day12 p2

--- src/day12/Main.purs
+++ src/day12/Main.purs
@@ -2,7 +2,7 @@ module Main where
 
 import Prelude
 
-import Data.Array (head, null, tail)
+import Data.Array (cons, head, null, tail)
 import Data.Int (fromString)
 import Data.Maybe (fromMaybe)
 import Data.Ord (abs)
@@ -34,7 +34,7 @@ main = do
     }
 
     instr = map convertinstr instructions
-
+  
     templateinstruction = {
                             itype: 'Q',
                             number: 0
@@ -80,8 +80,55 @@ main = do
         manhattendistance = if (null remainingInstructions)
                               then (abs eastDistance) + (abs northDistance)
                               else moveferry (northDistance + (movenorth - movesouth)) (eastDistance + (moveeast - movewest)) (newFacing) (fromMaybe [] $ tail remainingInstructions)
-                               
 
+    movewaypoint :: Int -> Int -> Int -> Int -> Array Instruction -> Int                        
+    movewaypoint shipn shipe waypointn waypointe remainingInstructions = manhattendistance
+      where
+        instruction = fromMaybe templateinstruction $ head remainingInstructions
+
+        movenorth = if instruction.itype == 'N'
+                      then instruction.number
+                      else 0
+        movesouth = if instruction.itype == 'S'
+                      then instruction.number
+                      else 0
+        moveeast = if instruction.itype == 'E'
+                      then instruction.number
+                      else 0
+        movewest = if instruction.itype == 'W'
+                      then instruction.number
+                      else 0
+
+        newwaypointNorth = if instruction.itype == 'L'
+                            then waypointe
+                            else 
+                              if instruction.itype == 'R' 
+                                then (0 - waypointe)
+                                else waypointn + (movenorth - movesouth)
+        newwaypointEast = if instruction.itype == 'L'
+                            then (0 - waypointn)
+                            else 
+                              if instruction.itype == 'R' 
+                                then waypointn
+                                else waypointe + (moveeast - movewest)
+
+        towardwaypointn = if instruction.itype == 'F'
+                            then shipn + (instruction.number * waypointn)
+                            else shipn
+        towardwaypointe = if instruction.itype == 'F'
+                            then shipe + (instruction.number * waypointe)
+                            else shipe
+                              
+
+        manhattendistance = if (null remainingInstructions)
+                              then (abs shipn) + (abs shipe)
+                              else 
+                                if (instruction.itype == 'R') && (instruction.number > 90)
+                                  then movewaypoint towardwaypointn towardwaypointe newwaypointNorth newwaypointEast (cons ({itype: 'R',number: (instruction.number - 90)}) (fromMaybe [] $ tail remainingInstructions))
+                                  else if (instruction.itype == 'L') && (instruction.number > 90)
+                                    then movewaypoint towardwaypointn towardwaypointe newwaypointNorth newwaypointEast (cons ({itype: 'L',number: (instruction.number - 90)}) (fromMaybe [] $ tail remainingInstructions))
+                                    else movewaypoint towardwaypointn towardwaypointe newwaypointNorth newwaypointEast (fromMaybe [] $ tail remainingInstructions)
+        
 
 
         
@@ -91,4 +138,4 @@ main = do
   S == 2
   W == 3
   -}
-  logShow $ moveferry 0 0 1 instr
+  logShow $ movewaypoint 0 0 1 10 instr
